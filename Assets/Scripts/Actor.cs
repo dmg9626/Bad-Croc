9509aeff0ead514bc4214a850292dbec70049b6a
Drew Graham <dmg346@drexel.edu>
2018-04-21 23:40:05 -0400

    creatd Actor and Player scripts, player can move right up to a max speed, camera follows player

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour
+{
+    /// <summary>
+    /// Health
+    /// </summary>
+    public int health;
+
+    /// <summary>
+    /// True if this is the Player
+    /// </summary>
+    public bool isPlayer;
+
+    /// <summary>
+    /// Movement speed
+    /// </summary>
+    public float moveSpeed;
+    
+    /// <summary>
+    /// Maximum movement speed
+    /// </summary>
+    public float maxSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Actor {
+
+    public Camera camera;
+
+	
+	// Update is called once per frame
+	void Update () {
+        HandleMovement();
+        Debug.Log("Velocity: " + GetComponent<Rigidbody2D>().velocity);
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z); 
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("user pressing D");
+            
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * moveSpeed);
+
+            if(GetComponent<Rigidbody2D>().velocity.magnitude >= maxSpeed)
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.right * maxSpeed;
+            }
+        }
+    }
+}
